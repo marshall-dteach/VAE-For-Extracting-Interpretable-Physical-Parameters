f8a564768165f2941e47c66e650b462fa0950fc9
teach <1250859983@qq.com>
2023-02-26 08:56:08 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Extracting-Interpretable-Physical-Parameters-from-Spatiotemporal-Systems-using-Unsupervised-Learning
+# 使用VAE提取PDE控制系统的关键参数
+
+使用VAE对PDE控制的物理系统进行关键参数提取
---
+++ npz_dataset.py
@@ -0,0 +1,52 @@
+"""
+npz_dataset.py
+
+Dataset class (for PyTorch DataLoader) for data saved in *.npz or *.npy format.
+If using a *.npz file, it must contain an array 'x' that stores all the data and
+can contain an optional array 'params' of known parameters for comparison.
+"""
+
+import paddle
+import numpy as np
+
+
+class PDEDataset(paddle.io.Dataset):
+    """PDE dataset with inputs x and targets also x."""
+
+    def __init__(self, data_file=None, transform=None, data_size=None):
+        """
+        Args:
+            data_file (numpy save): file with all data
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        data = np.load(data_file)
+
+        if type(data) is np.ndarray:
+            self.data_x = data
+            self.params = None
+        elif 'x' in data.files:
+            self.data_x = data['x']
+            self.params = data['params'] if 'params' in data.files else None
+        else:
+            raise ValueError("Dataset import failed. NPZ files must include 'x' array containing data.")
+
+        self.transform = transform
+
+
+    def __len__(self):
+        return len(self.data_x)
+
+    def __getitem__(self, idx):
+
+        x = paddle.to_tensor(self.data_x[idx])
+
+        if self.params is None:
+            sample = [x, x, paddle.to_tensor(float('nan'))]
+        else:
+            sample = [x, x, paddle.to_tensor(self.params[idx])]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
